7f8f09d7eef0665b8d3ef8cf7655fb14df362307
DramaneD <ddramane63@gmail.com>
2019-07-18 18:12:15 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@babel/polyfill'
+import App from './App'
+import './styles.css'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
+
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+)
